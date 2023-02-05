4c1572eca38ca259f294252e1990955d48d81ddd
zachyunl <jirizachar@seznam.cz>
2023-02-05 16:05:57 +0100

    new change

---
+++ Cvičení/dest1.js
@@ -0,0 +1,4 @@
+const cities = ["Praha", "Viden", "Olomouc", "Svitavy", "Zlin", "Ostrava"];
+const prices = [150, 200, 120, 120, 100, 180];
+console.log("Vítejte v aplikaci");
+console.log("------------------");
